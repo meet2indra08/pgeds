18020cec12c8a5906a7a45fb82df0f0c45aa6f61
kumin01@ca.com <kumin01@ca.com>
2017-09-06 02:40:11 -0400

    Git Hub Test

---
+++ src/main/java/com/sa/pgeds/PgedsApplication.java
@@ -0,0 +1,15 @@
+package com.sa.pgeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgedsApplication.class, args);
+		System.out.println("Running app");
+		
+	}
+	
+}
---
+++ src/main/java/com/sa/pgeds/PgedsContext.java
@@ -0,0 +1,5 @@
+package com.sa.pgeds;
+
+public class PgedsContext {
+
+}
